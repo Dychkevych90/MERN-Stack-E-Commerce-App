c2e245b3dae555147cf7a214dd4ccf22a2a363f4
mykola dychkevych <mykola.dychkevych@sigma.software>
2022-03-18 12:37:26 +0200

    added redux store

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
 
 import ReactDOM from 'react-dom';
+import {Provider} from "react-redux";
 import {BrowserRouter as Router} from "react-router-dom";
 
 import App from './components/app/App';
 
+import store from './store';
+
 import './index.css';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={ store }>
     <Router>
       <App />
     </Router>
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
